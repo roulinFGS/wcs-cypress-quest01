3981d7a695769cec97c5bf9fbf77565de4c919d8
fab <roulin.fabrice@gmail.com>
2024-05-22 11:12:18 +0200

    chore: setup e2e

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,12 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'a7bq2k', // TODO hide it
-})
+  // TODO hide it
+  projectId: "a7bq2k",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
